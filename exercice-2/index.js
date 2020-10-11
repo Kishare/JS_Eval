724b9557fc773b474be3abb781e32d9492ded560
Kishare <kicshary@gmail.com>
2020-10-11 03:12:53 +0200

    feat(drawbar): add drawbar

--- exercice-2/index.js
+++ exercice-2/index.js
@@ -1,1 +1,34 @@
 'use strict';
+
+/**
+ * Represents a drawbar.
+ * @constructor
+ * @param {number} sum - full-length bar.
+ * @param {number} nbr - loaded part bar.
+ */
+
+var drawBar = function drawBar(sum, nbr){
+    this.sum = sum;
+    this.nbr = nbr;
+}
+
+drawBar.prototype.run = function(){
+    this.display();
+}
+
+drawBar.prototype.display = function(){
+    var progress = document.createElement('div');
+    var bar = document.createElement('div');
+    progress.style.width = this.sum + '%';
+    progress.style.backgroundColor = 'lightgrey';
+    progress.style.height = '50px';
+    bar.style.width  = this.nbr + '%';
+    bar.style.backgroundColor = 'green';
+    bar.style.height = '50px';
+    progress.appendChild(bar);
+    document.body.appendChild(progress);
+
+}
+
+var loadingBar = new drawBar(100, 10);
+loadingBar.run();
