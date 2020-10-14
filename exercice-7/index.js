f414390cbfcc8b07a16283e03570054de15bc87b
Kishare <kicshary@gmail.com>
2020-10-14 19:11:15 +0200

    refactor(index.js): add jsdoc, modif indentation

--- exercice-7/index.js
+++ exercice-7/index.js
@@ -1,12 +1,18 @@
 'use strict';
 
+/**
+ * Represents a function that compute notes.
+ * @constructor
+ * @param {array} array - Multiples values in an array representing grades.
+ */
+
 var computeNotes = function computeNotes(array){
     this.array = array;
 }
 
-computeNotes.prototype.run = function(){
-    this.calculate();
-}
+/**
+ * Calculate the average of multiple values.
+ */
 
 computeNotes.prototype.calculate = function(){
     var array = this.array;
@@ -15,6 +21,14 @@ computeNotes.prototype.calculate = function(){
     moyArray += i;
     }
     console.log(moyArray);
+}
+
+/**
+ * Run
+ */
+
+checkPhoneNumber.prototype.run = function(){
+    this.check();
 }
 
 var moy1 = new computeNotes([10, 13, 13, 12, 15, 12, 11, 16, 14]);
